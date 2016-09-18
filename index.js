70863c8159915c2f084923bdc290dd5ec0b00ae6
vinitm <my_name_is97@yahoo.com>
2016-09-18 22:04:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var cloudscraper = require('cloudscraper');
+var config = require('./config');
+var querystring = require('querystring');
+var app = express();
+var DataExtracter = require('./modules/DataExtracter');
+
+app.get('/', function(req, res) {
+	var query = querystring.stringify(req.query);
+	cloudscraper.get(config.url + '/?' + query, function(error, response, html) {
+		if (error) {
+			console.log(error);
+			response.end(JSON.stringify(error));
+		} else {
+			console.log('body received');
+			DataExtracter.extractFrom(html).send(res);
+		}
+	});
+});
+
+app.listen(config.port, function() {
+	console.log('Example app listening on port ' + config.port + '!');
+});
---
+++ modules/DataExtracter.js
@@ -0,0 +1,32 @@
+var cheerio = require('cheerio');
+
+var _extract = function(html) {
+	var obj = {};
+	var $ = cheerio.load(html);
+	obj.torrents = [];
+	$('.torrent').each(function() {
+		var torrent = $(this);
+		var torrentObj = {};
+		torrentObj.title = torrent.find('.r a').text().trim();
+		torrentObj.seeds = torrent.find('.seeders .gac_b').text().trim();
+		torrentObj.leechs = torrent.find('.leechers .gac_b').text().trim();
+		torrentObj.time_uploaded = torrent.find('.cated').text().trim();
+		torrentObj.category = torrent.find('.cate').text().trim();
+		torrentObj.size = torrent.find('.torrent-size').text().trim().toUpperCase().trim();
+		torrentObj.magnet = 'magnet:?xt=urn:btih:' + torrent.find('.r a').attr('href').split('/')[1].trim() + '&dn=' + torrentObj.title.replace(/ /g, '_');
+		obj.torrents.push(torrentObj);
+	});
+	return obj;
+};
+
+module.exports = {
+	extractFrom: function(html) {
+		this.json = _extract(html);
+		return this;
+	},
+	send: function(response) {
+		response.setHeader('Content-Type', 'application/json');
+		response.end(JSON.stringify(this.json));
+		return this;
+	}
+};
